332c3093364c261e484c510fc6a812c28c8dfb3e
Victoria Cheng <victoria.cheng15@gmail.com>
2023-02-25 15:30:07 -0700

    feat: add rome formatter and format.yml

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
-
 function App() {
-
-  return (
-    <div className="App">
-      <h1 className='text-blue-900 font-bold text-3xl underline'>Hello from TailwindCSS</h1>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1 className='text-blue-900 font-bold text-3xl underline'>
+				Hello from TailwindCSS
+			</h1>
+		</div>
+	);
 }
 
-export default App
+export default App;
