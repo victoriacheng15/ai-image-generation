c66c07f4143515f6e9869b47d28a9934f11e159b
victoriacheng15 <victoriacheng15@users.noreply.github.com>
2023-02-26 00:02:36 +0000

    style: the code has been formatted by rome!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,32 +3,32 @@ import { CreatePost, Home } from "./pages";
 import logo from "./assets/logo.svg";
 
 function App() {
-  return (
-    <>
-      <header className="bg-blue-200 py-4">
-        <div className="w-11/12 mx-auto flex justify-between items-center">
-          <Link to="/">
-            <img src={logo} alt="logo" className="w-28 object-contain" />
-          </Link>
+	return (
+		<>
+			<header className="bg-blue-200 py-4">
+				<div className="w-11/12 mx-auto flex justify-between items-center">
+					<Link to="/">
+						<img src={logo} alt="logo" className="w-28 object-contain" />
+					</Link>
 
-          <Link
-            to="/create-post"
-            className="font-inter font-medium bg-blue-900 text-slate-100 px-4 py-2 rounded-md"
-          >
-            Create
-          </Link>
-        </div>
-      </header>
-      <main className="min-h-[calc(100vh-72px)] py-8">
-        <div className="w-11/12 mx-auto">
-          <Routes>
+					<Link
+						to="/create-post"
+						className="font-inter font-medium bg-blue-900 text-slate-100 px-4 py-2 rounded-md"
+					>
+						Create
+					</Link>
+				</div>
+			</header>
+			<main className="min-h-[calc(100vh-72px)] py-8">
+				<div className="w-11/12 mx-auto">
+					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/create-post" element={<CreatePost />} />
 					</Routes>
-        </div>
-      </main>
-    </>
-  );
+				</div>
+			</main>
+		</>
+	);
 }
 
 export default App;
