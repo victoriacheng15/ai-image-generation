57df752ebcdebd262f8bf484f83afeac537f6ff5
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-02-27 16:14:45 -0700

    Add server files (#6)
    * chore: set type to module
    
    * feat: add connect.js for mongodb server connection
    
    * feat: add post model
    
    * feat: add server.js
    
    * feat: add dalleRoutes
    
    * feat: add postRoutes
    
    * style: the code has been formatted by rome!
    
    ---------
    
    Co-authored-by: victoriacheng15 <victoriacheng15@users.noreply.github.com>

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+
+dotenv.config();
+
+const PROT = process.env.PROT || 8080;
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+// app.use("/api/v1/posts", postRoutes);
+// app.use("/api/v1/dalle", dalleRoutes);
+
+app.get("/", async (req, res) => {
+	res.send("Hello from DALL-E");
+});
+
+(async function startServer() {
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(PROT, () => {
+			console.log(`Server is running on port ${PROT}`);
+		});
+	} catch (error) {
+		console.error(error);
+	}
+})();
