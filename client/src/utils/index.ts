0c151a17ac7e93df6a4e9affe26b8f075f4486f8
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-02-28 17:18:27 -0700

    feat(main): add (client/server) on each commit message (#9)
    * feat(main): add (client/server) on each commit message
    
    * style(client): the code has been formatted by rome!
    
    ---------
    
    Co-authored-by: victoriacheng15 <victoriacheng15@users.noreply.github.com>

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,4 +1,4 @@
-import FileSaver from "file-saver"
+import FileSaver from "file-saver";
 import { surpriseMePrompts } from "../constants";
 
 export function getRandomPrompt(prompt: string): string {
