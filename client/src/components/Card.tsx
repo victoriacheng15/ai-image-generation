faa5890185d977a3d2da12b6ce0a4d120d7a3699
Victoria Cheng <victoria.cheng15@gmail.com>
2023-02-28 17:14:29 -0700

    feat(client): add elements and css to render card

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { AiOutlineDownload } from "react-icons/ai";
+import { downloadImage } from "../utils";
 
-function Card() {
-	return <div>Card</div>;
+function Card(
+  { _id, name, prompt, photo }: {
+    _id: string;
+    name: string;
+    prompt: string;
+    photo: string;
+  },
+) {
+  return (
+    <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
+      <img
+        src={photo}
+        alt={prompt}
+      />
+      <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
+        <p className="text-white text-sm overflow-y-auto prompt">{prompt}</p>
+
+        <div className="mt-5 flex justify-between items-center gap-2">
+          <div className="flex items-center gap-2">
+            <div className="w-7 h-7 rounded-full object-cover bg-blue-700 flex justify-center items-center text-white text-xs font-bold">
+              {name[0]}
+            </div>
+            <p className="text-white text-sm">{name}</p>
+          </div>
+          <button
+            type="button"
+            onClick={() => downloadImage(_id, photo)}
+            className="outline-none bg-transparent border-none"
+          >
+            <AiOutlineDownload className="bg-blue-300 text-2xl rounded-full p-1" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
