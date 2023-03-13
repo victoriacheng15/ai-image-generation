8ef4e80b6643d893a1944d75777e6f765a05aaf8
Victoria Cheng <victoria.cheng15@gmail.com>
2023-03-13 08:57:57 -0600

    feat: add getAllPosts and createPost controllers

---
+++ server/controllers/postController.js
@@ -0,0 +1,41 @@
+import * as dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+import Post from "../models/post.js";
+
+dotenv.config();
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export async function getAllPosts(req, res) {
+  try {
+		const posts = await Post.find();
+		res.status(200).json({ success: true, data: posts });
+	} catch (error) {
+		res.status(500).json({ success: false, error: error });
+	}
+}
+
+export async function createPost(req, res) {
+  try {
+		const { name, prompt, photo } = req.body;
+		const photoUrl = await cloudinary.uploader.upload(photo, {
+			folder: "posts",
+		});
+
+		const newPost = new Post({
+			name,
+			prompt,
+			photo: photoUrl.url,
+		});
+
+		await newPost.save();
+
+		res.status(201).json({ success: true, data: newPost });
+	} catch (error) {
+		res.status(500).json({ success: false, error: error.message });
+	}
+}
